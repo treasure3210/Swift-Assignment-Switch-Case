b0c81aa5f9fa9504dbc7021e61b25ed58937d470
[treasure3210] <treasure3210@gmail.com>
2023-01-11 12:19:22 -0700

    Complete

--- Switch Case.playground/Contents.swift
+++ Switch Case.playground/Contents.swift
@@ -27,3 +27,39 @@ case 7:
 default: break
 }
 
+/// 2. Input month number integer and display month name in the word
+/// Input:
+///     Input month number: 1
+/// Output:
+///     January
+
+var monthOfTheYear = 1
+
+switch monthOfTheYear {
+case 1:
+    print("January")
+case 2:
+    print("February")
+case 3:
+    print("March")
+case 4:
+    print("April")
+case 5:
+    print("May")
+case 6:
+    print("June")
+case 7:
+    print("July")
+case 8:
+    print("August")
+case 9:
+    print("September")
+case 10:
+    print("October")
+case 11:
+    print("November")
+case 12:
+    print("December")
+default:
+    break
+}
