b294277abbc18174c0a306731f4fcf20c39341f9
[treasure3210] <treasure3210@gmail.com>
2023-01-11 12:15:26 -0700

    Initial Commit

---
+++ Switch Case.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+/// Switch Case
+/// 1. Input day number in integer and display day name in the word
+/// Input:
+///     Input day number: 1
+/// Output:
+///     Monday
+
+var daysOfTheWeek = 1
+
+switch daysOfTheWeek {
+case 1:
+    print("Monday")
+case 2:
+    print("Tuesday")
+case 3:
+    print("Wednesday")
+case 4:
+    print("Thursday")
+case 5:
+    print("Friday")
+case 6:
+    print("Saturday")
+case 7:
+    print("Sunday")
+default: break
+}
+
